e85fb41d612677889c666edefe79958874c88641
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-20 20:49:15 -0500

    added clear function, start of populaytedisplay and getresult

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let number1 = 0;
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector("#display");
+const operators = document.querySelector("#operators");
+
+
+let number1 = [];
 let operator = "";
-let number2 = 0;
+let number2 = [];
 
 function add(num1, num2) {
     return num1 + num2;
@@ -21,3 +26,34 @@ function divide(num1, num2) {
 function operate(op, num1, num2) {
     return op(num1, num2);
 }
+
+function clear() {
+    display.textContent = 0;
+    number1 = [];
+    operator = "";
+    number2 = [];
+}
+
+
+function getResult() {
+
+    numbers.forEach(element => {
+        element.addEventListener("click", (e) => {
+            number1.push(e.textContent);
+            populateDisplay(number1)
+        })
+    });
+
+    operators.forEach(element => {
+        element.addEventListener("click", (e) => {
+            
+        })
+    });
+}
+
+function populateDisplay(array) {
+    let result = array.join("");
+    display.textContent = result;
+}
+
+populateDisplay();
